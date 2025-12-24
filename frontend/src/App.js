92fdcdb4cb10260b7c17e18c6c589bc0ad8edbf7
bader baheshwan <bader.b2001@gmail.com>
2025-12-24 08:45:18 +0300

    fixed frontend build error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from './Login';
 import Dashboard from './Dashboard';
 
